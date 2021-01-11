1a929df25bd761e82aa0ff0c33b22a18f6d7cc9b
l.jackson96 <l.jackson96@outlook.com>
2021-01-11 17:30:23 -0600

    finished 3 links css

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
@@ -13,9 +14,33 @@ function Header() {
           alt=""
         />
       </Link>
-      {/** Search box */}
-      <input type="text" className="header_searchInput" />
+      <div className="header_search">
+        {/** Search box */}
+        <input type="text" className="header_searchInput" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
       {/** 3 Links */}
+      <div className="header_nav">
+        <Link to="/login" className="header_link">
+          <div className="header_option">
+            <span className="header_optionLineOne">Hello JR</span>
+            <span className="header_optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header_link">
+          <div className="header_option">
+            <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        <Link to="/" className="header_link">
+          <div className="header_option">
+            <span className="header_optionLineOne">Your</span>
+            <span className="header_optionLineTwo">Prime</span>
+          </div>
+        </Link>
+      </div>
       {/** Basket icon w/ updated number */}
     </nav>
   );
