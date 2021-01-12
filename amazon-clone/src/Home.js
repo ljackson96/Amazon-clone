3e5c87bb8fc8ba0b6f0440b10bb815d62c59b7f4
l.jackson96 <l.jackson96@outlook.com>
2021-01-12 12:15:42 -0600

    added landing image + css

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,17 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Home.css";
 import Product from "./Product";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  const options = {
+    method: "GET",
+    url:
+      "https://amazon-product-reviews-keywords.p.rapidapi.com/product/search",
+    params: { keyword: "board games", country: "US", category: "aps" },
+    headers: {
+      "x-rapidapi-key": "ca036601damsh5819a2bc9ae9cffp14861fjsncd852ab3030f",
+      "x-rapidapi-host": "amazon-product-reviews-keywords.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then(function (response) {
+        //console.log(response.data.products);
+        setProducts(response.data.products);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
   return (
     <div className="home">
       <img
         className="home_image"
-        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/Brand/JD/2021/Jan_default_onsite/XCM_Manual_1302634-gw_desk_tall-control-en-2x_56692bd7-443d-4ff8-a7c2-09ed2ae16e64._CB411473854_.png"
         alt=""
       />
       {/* Product id, title, price, rating, image */}
-      <Product id="123" title="test" price={11.23} rating={5} image="/" />
+      <div className="home_row">
+        <Product
+          id="123"
+          title="test"
+          price={11.23}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/81iADdtXixL._AC_UL320_.jpg"
+        />
+        <Product
+          id="123"
+          title="test"
+          price={11.23}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/81iADdtXixL._AC_UL320_.jpg"
+        />
+      </div>
+      <div className="home_row">
+        <Product
+          id="123"
+          title="test"
+          price={11.23}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/81iADdtXixL._AC_UL320_.jpg"
+        />
+        <Product
+          id="123"
+          title="test"
+          price={11.23}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/81iADdtXixL._AC_UL320_.jpg"
+        />
+        <Product
+          id="123"
+          title="test"
+          price={11.23}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/81iADdtXixL._AC_UL320_.jpg"
+        />
+      </div>
+      <div className="home_row">
+        <Product
+          id="123"
+          title="test"
+          price={11.23}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/81iADdtXixL._AC_UL320_.jpg"
+        />
+      </div>
+
       {/* Product */}
     </div>
   );
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -4,19 +4,21 @@ import "./Product.css";
 function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
-      <p>{title}</p>
-      <p className="product_price">
-        <small>$</small>
-        <strong>{price}</strong>
-      </p>
-      <div className="product_rating">
-        {Array(rating)
-          .fill()
-          .map((_) => (
-            <p>⭐️</p>
-          ))}
+      <div className="product_info">
+        <p>{title}</p>
+        <p className="product_price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product_rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>⭐️</p>
+            ))}
+        </div>
       </div>
-      <img src="/" alt="" />
+      <img src={image} alt={id} />
       <button>Add to Basket</button>
     </div>
   );
