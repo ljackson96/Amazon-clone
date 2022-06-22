39928b0f17e23f43b3f1cb7d9d839798a913a8d9
Lawrence Jackson <60304583+ljackson96@users.noreply.github.com>
2022-06-21 23:49:15 -0500

    fixed addtobasket onclick

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -35,7 +35,7 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt={id} />
-      <button onClick={addToBasket}>Add to Basket</button>
+      <button onClick={() => addToBasket}>Add to Basket</button>
     </div>
   );
 }
